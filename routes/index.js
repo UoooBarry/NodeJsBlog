74d90080d1c513a1057ef5316778f9dca094a5f2
UoooBarry <s3719368@student.rmit.edu.au>
2020-06-25 14:37:40 +1000

    user already duplicate validation

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 var passwordHash = require('password-hash');
-const { check,validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const session_helper = require('../helpers/session_helper');
 
 /* GET home page. */
@@ -21,8 +21,21 @@ router.get('/register', function(req, res){
 router.post('/register',
 [
   check('name').not().isEmpty().withMessage('Name cannot be empty'),
+  check('name').custom(async function(value){
+     var user = await User.findOne({name: value});
+     if(user){
+       return Promise.reject();
+     }
+  }).withMessage('Username already exists'),
   check('contact').isLength({min:10, max:10}).withMessage('Contact number should be 10 digits').matches(/^[0][0-9]*$/).withMessage('Contact must be numbers start with 0'),
-  check('password').isLength({min: 6}).withMessage('Password need to be more than 10 digits')
+  check('password').isLength({min: 6}).withMessage('Password need to be more than 10 digits'),
+  check('password').custom(function(val,{req}){
+    if(val !== req.body.confirm_password){
+      throw new Error("Password not matched");
+    }else{
+        return val;
+    }
+  })
 ]
 ,function(req,res){
   var errors = validationResult(req);
